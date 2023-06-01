ff8e6b510857873a79f1739ef60c930c255084c9
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-06-01 04:05:28 +0000

    [start]5-4

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,11 +4,7 @@ class FavoritesController < ApplicationController
         favorite = current_user.favorites.new(book_id: book.id)
         if favorite.save
             # request.refererを使用して直前のURLを取得し、そのURLがbook_path(book)と一致するかどうかをチェック
-            if request.referer == book_url(book)
-              redirect_to book_path(book)
-            else
-              redirect_to books_path
-            end
+            redirect_back fallback_location: root_path
         else
           redirect_to book_path(book)
         end
