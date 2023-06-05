b5fff0ca76ace99f6bd80837d5f4712747cd20c1
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-06-05 05:04:23 +0000

    [start]5-6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,7 @@ class FavoritesController < ApplicationController
             # request.refererを使用して直前のURLを取得し、そのURLがbook_path(book)と一致するかどうかをチェック
             redirect_back fallback_location: root_path
         else
-          redirect_to book_path(book)
+          redirect_to request.referrer
         end
     end
     
@@ -15,12 +15,13 @@ class FavoritesController < ApplicationController
         favorite = current_user.favorites.find_by(book_id: book.id)
         if favorite.destroy
           if request.referer == book_url(book)
-            redirect_to book_path(book)
+            redirect_to request.referrer
           else
-            redirect_to books_path
+            redirect_to request.referrer
+
           end
         else
-          redirect_to book_path(book)
+          redirect_to request.referrer
         end
     end
 end
