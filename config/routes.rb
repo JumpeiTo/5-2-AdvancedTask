c5daad342043c94110c1ca5263e81a63531df135
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-06-09 07:48:13 +0000

    [finish]5-8c

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   
   resources :messages, only: [:create]
   resources :rooms, only: [:create,:show]
-  resources :groups, except: [:destroy]
+  resources :groups do
+     get "join" => "groups#join"
+  end
 
 end
